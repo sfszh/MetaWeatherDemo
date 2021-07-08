52825a953b84f34f97fef3bcc11d5185ac716920
Rui Zhang <rayruizhang1@gmail.com>
2021-07-08 13:43:19 +0800

    add Dependencies.kt and clean the project make it ready to start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -40,23 +42,73 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerExtensionVersion = co.ruizhang.buildsrc.Libs.AndroidX.Compose.version
         kotlinCompilerVersion = "1.4.32"
     }
 }
-
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha08")
-    testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    implementation(co.ruizhang.buildsrc.Libs.Kotlin.stdlib)
+    implementation(co.ruizhang.buildsrc.Libs.Coroutines.android)
+
+    implementation(co.ruizhang.buildsrc.Libs.Hilt.hilt)
+    kapt(co.ruizhang.buildsrc.Libs.Hilt.androidCompiler)
+    kapt(co.ruizhang.buildsrc.Libs.Hilt.compiler)
+    implementation(co.ruizhang.buildsrc.Libs.Hilt.viewModel)
+    implementation(co.ruizhang.buildsrc.Libs.Hilt.composeNavigation)
+
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.coreKtx)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.navigation)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Activity.activityCompose)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.ConstraintLayout.constraintLayoutCompose)
+
+    //region compose
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.runtime)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.foundation)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.layout)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.ui)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.uiUtil)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.viewModel)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.liveData)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.material)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.animation)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.iconsExtended)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.tooling)
+    //endregion
+
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Lifecycle.liveDataKtx)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Lifecycle.runTime)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Lifecycle.viewModelKtx)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.Lifecycle.extensions)
+    implementation(co.ruizhang.buildsrc.Libs.AndroidX.DataStore.preferences)
+
+    implementation(co.ruizhang.buildsrc.Libs.Accompanist.coil)
+    implementation(co.ruizhang.buildsrc.Libs.Accompanist.insets)
+
+    implementation(co.ruizhang.buildsrc.Libs.Retrofit.retrofit)
+    implementation(co.ruizhang.buildsrc.Libs.Retrofit.gson)
+
+    //region room
+    implementation(co.ruizhang.buildsrc.Libs.Room.runtime)
+    annotationProcessor(co.ruizhang.buildsrc.Libs.Room.compiler)
+    kapt(co.ruizhang.buildsrc.Libs.Room.compiler)
+    implementation(co.ruizhang.buildsrc.Libs.Room.ktx)
+    //endregion
+
+    //region Test
+    testImplementation(co.ruizhang.buildsrc.Libs.JUnit.junit)
+    testImplementation(co.ruizhang.buildsrc.Libs.MockK.mockk)
+    testImplementation(co.ruizhang.buildsrc.Libs.Coroutines.test)
+    testImplementation(co.ruizhang.buildsrc.Libs.Hilt.test)
+    kaptTest(co.ruizhang.buildsrc.Libs.Hilt.test)
+    testImplementation(co.ruizhang.buildsrc.Libs.Arch.testing)
+
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Activity.activityCompose)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Test.core)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Test.espressoCore)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Test.rules)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Test.Ext.junit)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.AndroidX.Compose.uiTest)
+    androidTestImplementation(co.ruizhang.buildsrc.Libs.Room.testing)
+    //endregion
 }
---
+++ buildSrc/src/main/java/co/ruizhang/buildsrc/Dependencies.kt
@@ -0,0 +1,134 @@
+
+package co.ruizhang.buildsrc
+
+object Versions {
+    const val ktlint = "0.41.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta05"
+
+    object Accompanist {
+        const val version = "0.9.0"
+        const val coil = "com.google.accompanist:accompanist-coil:$version"
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+    }
+
+    object Arch {
+        private const val version = "1.1.1"
+        const val testing = "android.arch.core:core-testing:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.4.32"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.4.2"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val gson = "com.squareup.retrofit2:converter-gson:$version"
+    }
+
+    object Room {
+        private const val version = "2.3.0"
+        const val runtime = "androidx.room:room-runtime:$version"
+        const val compiler = "androidx.room:room-compiler:$version"
+        const val ktx = "androidx.room:room-ktx:$version"
+        const val testing = "androidx.room:room-testing:$version"
+    }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
+    object Mockito {
+        const val core = "org.mockito:mockito-core:1.10.19"
+        const val kotlin = "org.mockito.kotlin:mockito-kotlin:3.2.0"
+    }
+
+    object MockK {
+        const val mockk = "io.mockk:mockk:1.11.0"
+    }
+
+    object Hilt {
+        private const val version = "2.37"
+        private const val miscVersion = "1.0.0-alpha03"
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val hilt = "com.google.dagger:hilt-android:$version"
+        const val androidCompiler = "com.google.dagger:hilt-android-compiler:$version"
+        const val viewModel = "androidx.hilt:hilt-lifecycle-viewmodel:$miscVersion"
+        const val compiler = "androidx.hilt:hilt-compiler:$miscVersion"
+        const val test = "com.google.dagger:hilt-android-testing:$version"
+        const val composeNavigation = "androidx.hilt:hilt-navigation-compose:1.0.0-alpha02"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha01"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha07"
+        }
+
+        object Compose {
+            const val snapshot = ""
+            const val version = "1.0.0-beta07"
+
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val uiUtil = "androidx.compose.ui:ui-util:$version"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha05"
+            const val liveData = "androidx.compose.runtime:runtime-livedata:$version"
+            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.3.1"
+            const val runTime = "androidx.lifecycle:lifecycle-runtime:$version"
+            const val liveDataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
+            const val extensions= "androidx.lifecycle:lifecycle-extensions:2.2.0"
+            const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+
+        }
+
+        object ConstraintLayout {
+            const val constraintLayoutCompose =
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05"
+        }
+        object DataStore {
+            private const val version =  "1.0.0-beta01"
+            const val preferences = "androidx.datastore:datastore-preferences:$version"
+        }
+
+        object Test {
+            private const val version = "1.3.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0"
+        }
+    }
+}
